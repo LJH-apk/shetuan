0fa39570d58a420ee675bd5255da18f3ab7a4863
LJH-apk <18701059325@qq.com>
2020-04-03 21:24:51 +0800

    Create Text.java

---
+++ Text.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * @author 刘佳航
+ * @date 2020/4/3  19:57
+ */
+public class Text {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        //真正的算法交给你了
+
+
+
+
+    }
+}
