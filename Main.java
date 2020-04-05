cf7a38c0388d1e2338a9e13db3d854d62af4bb0c
LJH-apk <18701059325@qq.com>
2020-04-05 10:30:31 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+/**
+ * @author 刘佳航
+ * @date 2020/4/5  10:28
+ */
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        double number1 = scanner.nextDouble();
+        double number2 = scanner.nextDouble();
+        //TODO
+    }
+}
